6da553d1fa26964afe90c2e0aba6faf7d5af4d5b
chamindu20081403 <chamindu200804@gmail.com>
2025-02-28 15:55:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "G7YwFa7D#xA9rHOXUyBmKjlkSczEvxQ3Z3pFGIpOOgy1uLcsqGs8,
+  SESSION_ID: process.env.SESSION_ID || "G7YwFa7D#xA9rHOXUyBmKjlkSczEvxQ3Z3pFGIpOOgy1uLcsqGs8",
   MONGODB: process.env.MONGODB || "mongodb://mongo:gypcdIlYLLwTrZUhrdhhNLtAQkzVBOQh@switchback.proxy.rlwy.net:45570",
   OWNER_NUM: process.env.OWNER_NUM || "94783314361",
 };
